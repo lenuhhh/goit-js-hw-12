3e832c61366e6d19bbc9eee2a6dddd4f9122849e
lenuhhh <161879235+lenuhhh@users.noreply.github.com>
2024-10-03 14:48:40 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const returnPromise = (q, page) => {
 return axios.get('https://pixabay.com/api/', {
     params: {
-    key: '46086824-2983fdd94aba44351510a0e76',
+  key :"34668694-a67447f729748c64ab9ad3b4f",
     q,
     orientation: 'horizontal',
     safesearch: true,
